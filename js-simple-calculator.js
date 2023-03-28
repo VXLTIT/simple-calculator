378b92149009a2bd114fe37f1d2654a387942c0a
VXLTIT <danil050706@gmail.com>
2023-03-28 08:34:07 +0500

    -added calculation history

--- js-simple-calculator.js
+++ js-simple-calculator.js
@@ -1,6 +1,10 @@
 
 
-let sign, resultText, resultNumber, number1, number2, resultButton, count, clearButton;
+// declaration of variables and values
+
+
+let sign, resultText, resultNumber, number1, historyCalc, articleBack,
+    number2, resultButton, count, clearButton, div, para, historyCalcText;
 
 resultText = document.querySelector('.result-text');
 resultNumber = document.querySelector('.result-number');
@@ -10,13 +14,19 @@ sign = document.querySelector('#sign');
 resultButton = document.querySelector('.show-result');
 articleBack = document.querySelector('article');
 clearButton = document.querySelector('.clear');
+historyCalc = document.querySelector('.calculation-history');
+historyCalcText = document.querySelector('h3');
 count = 0;
+
+// button operation
 
 resultButton.addEventListener('click', calculate);
 clearButton.addEventListener('click', clearFields);
 
 
 
+
+// functions responsible for the operation of the calculator
 
 function calculate () {
 
@@ -28,36 +38,41 @@ function calculate () {
             case 'summarized':
                 resultNumber.textContent = num1 + num2;
                 showResultToUser()
+                addCalculationToHistory();
                 break;
+
             case 'difference':
                 resultNumber.textContent = num1 - num2;
                 showResultToUser()
+                addCalculationToHistory();
                 break;
+
             case 'multiplication':
                 resultNumber.textContent = num1 * num2;
                 showResultToUser()
+                addCalculationToHistory();
                 break;
+
             case 'division':
                 if (num2 != 0) {
                 resultNumber.textContent = num1 / num2;
                 showResultToUser()
+                addCalculationToHistory();
                 } else {
                     resultNumber.textContent = 'На ноль делить нельзя'
                 }
                 break;
+
             default:
                 resultNumber.textContent = 'Выберите Знак';
                 resultNumber.style.display = 'block';
                 resultText.style.display = 'none';
-                articleBack.style.height = '15rem';
         }
 
     } else {
         resultNumber.textContent = 'Введите число!';
         resultText.style.display = 'none';
         resultNumber.style.display = 'block';
-        articleBack.style.height = '15rem';
-
     }
 
 }
@@ -65,11 +80,46 @@ function calculate () {
 function showResultToUser() {
     resultText.style.display = 'block';
     resultNumber.style.display = 'block';
-    articleBack.style.height = '21rem';
 }
 
 function clearFields() {
     number1.value = '';
     number2.value = '';
     sign.value = 'choose'
+}
+
+// functions for calculation history
+
+function addCalculationToHistory() {
+    switch (sign.value) {
+        case 'summarized':
+            createParagraph();
+            para.textContent = `${num1} + ${num2} = ${resultNumber.textContent}`;
+            break;
+
+        case 'difference':
+            createParagraph();
+            para.textContent = `${num1} - ${num2} = ${resultNumber.textContent}`;
+            break;
+
+        case 'multiplication':
+            createParagraph();
+            para.textContent = `${num1} \u00D7 ${num2} = ${resultNumber.textContent}`;
+            break;
+
+        case 'division':
+            createParagraph();
+            para.textContent = `${num1} \u00F7 ${num2} = ${resultNumber.textContent}`;
+            break;
+    };
+}
+
+function createParagraph() {
+    div = document.createElement('div');
+    para = document.createElement('p');
+    historyCalc.appendChild(div);
+    div.appendChild(para);
+    historyCalcText.style.display = 'block';
+    div.style.backgroundColor = 'grey';
+    articleBack.style.paddingBottom = '1rem';
 }
